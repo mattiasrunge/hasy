e8e4aad86b2e8eb5d28b4aa19d46f81b7d4bca73
Mattias Runge <mattias@runge.se>
2015-07-01 21:25:18 +0200

    Stuff

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 "use strict";
 
-var argv = require("yargs").argv;
-var Hasy = require("./lib/hasy");
+let argv = require("yargs").argv;
+let Hasy = require("./lib/hasy");
 
-var hasy = new Hasy();
+let hasy = new Hasy();
+
+// var gkm = require('gkm');
+//
+// // Listen to all key events (pressed, released, typed)
+// gkm.events.on('key.*', function(data) {
+//     console.log(this.event + ' ' + data);
+// });
+
+// Listen to all mouse events (click, pressed, released, moved, dragged)
+// gkm.events.on('mouse.*', function(data) {
+//     console.log(this.event + ' ' + data);
+// })
 
 hasy.start(argv);
