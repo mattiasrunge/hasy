ea4556a35f391cc0e5c97a2ba0c75b521732bfc3
Mattias Runge <mattias@runge.se>
2015-07-07 20:15:48 +0200

    Small stuff

--- control/policies/mattias/media.js
+++ control/policies/mattias/media.js
@@ -1,15 +1,17 @@
 "use strict";
 
 module.exports = function(control) {
-    control.onPropertyChange("pbutton", "Button", function*(event) {
+    control.onPropertyChange("button", "Button", function*(event) {
         let actions = [];
 
         if (event.newValue === "M") {
             actions.push(control.setProperty("yamahareceiver", "Source", "HDMI1"));
             actions.push(control.setProperty("vardagsrumstak", "Value", 0));
+            actions.push(control.setProperty("vardagsrumstv", "Source", "HDMI2"));
         } else if (event.newValue === "T") {
             actions.push(control.setProperty("yamahareceiver", "Source", "AV1"));
             actions.push(control.setProperty("vardagsrumstak", "Value", 255));
+            actions.push(control.setProperty("vardagsrumstv", "Source", "DTV"));
         }
 
         return actions;
