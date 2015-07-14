52c61af9f4a90958c681910a5b72c54f17054f5b
Mattias Runge <mattias@runge.se>
2015-07-14 22:17:55 +0200

    Fixed policies again

--- policies/mattias/media.js
+++ policies/mattias/media.js
@@ -1,19 +1,17 @@
 "use strict";
 
 module.exports = function(properties) {
-    control.onPropertyChange("button", "Button", function*(event) {
-        let actions = [];
-
-        if (event.newValue === "M") {
-//             actions.push(control.setProperty("yamahareceiver", "Source", "HDMI1"));
-            actions.push(control.setProperty("vardagsrumstak", "Value", 0));
-//             actions.push(control.setProperty("vardagsrumstv", "Source", "HDMI2"));
-        } else if (event.newValue === "T") {
-//             actions.push(control.setProperty("yamahareceiver", "Source", "AV1"));
-            actions.push(control.setProperty("vardagsrumstak", "Value", 255));
-//             actions.push(control.setProperty("vardagsrumstv", "Source", "DTV"));
+    properties.onEvent(function(event, data) {
+        if (event === "propertyChange" && data.unitId === "button" && data.data.name === "Data") {
+            if (data.data.newValue === "KEY_O") {
+    //             actions.push(control.setProperty("yamahareceiver", "Source", "HDMI1"));
+                properties.set("livingroom-ceiling", "Value", 0);
+    //             actions.push(control.setProperty("vardagsrumstv", "Source", "HDMI2"));
+            } else if (data.data.newValue === "KEY_P") {
+    //             actions.push(control.setProperty("yamahareceiver", "Source", "AV1"));
+                properties.set("livingroom-ceiling", "Value", 255);
+    //             actions.push(control.setProperty("vardagsrumstv", "Source", "DTV"));
+            }
         }
-
-        return actions;
     });
 };
